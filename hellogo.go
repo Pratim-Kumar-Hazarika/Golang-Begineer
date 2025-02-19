1dc511eba3390ac6e9919d1a76f81d5d5e40dfb7
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-02-20 01:05:11 +0530

    Initial commit

---
+++ hellogo.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+var pl = fmt.Println
+
+func main() {
+	pl("What is your name?")
+	reader := bufio.NewReader(os.Stdin)
+	name, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal(err)
+	}
+	name = strings.TrimSpace(name) // Removes the trailing newline
+	pl("Hello", name)
+}
