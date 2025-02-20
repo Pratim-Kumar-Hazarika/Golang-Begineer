5a4a84330ab2474ef9818e6d0eecd67f8a1b0b4d
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-02-21 01:13:19 +0530

    Update hellogo.go

--- hellogo.go
+++ hellogo.go
@@ -20,7 +20,18 @@ var pl = fmt.Println
 //Casting
 func main(){
 	//Convert string
-	cv1 := "5000000000"
-	 cv2 , err := strconv.Atoi(cv1)
-	 pl(cv2, err, reflect.TypeOf(cv2))
+	// cv1 := "5000000000"
+	//  cv2 , err := strconv.Atoi(cv1)
+	//  pl(cv2, err, reflect.TypeOf(cv2))
+
+	// cV1 := 5000000000
+	// cv2 := strconv.Itoa(cV1)
+	// pl(cv2,reflect.TypeOf(cv2))
+
+	//Convert to float
+	cv1 := "3.14"
+	if cv1 , err := strconv.ParseFloat(cv1, 64);
+	err == nil{
+		pl(cv1, reflect.TypeOf(cv1));
+	}
 }
