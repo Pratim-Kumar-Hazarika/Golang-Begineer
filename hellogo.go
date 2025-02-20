12450b67fff3be663c237d10b92c141748843acc
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-02-21 01:08:24 +0530

    add casting

--- hellogo.go
+++ hellogo.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 //alias
 var pl = fmt.Println
 
-func main() {
-	pl(reflect.TypeOf(42))
-	pl(reflect.TypeOf(3.1415))
-	pl(reflect.TypeOf(3 + 4i))
-	pl(reflect.TypeOf("hello"))
-	pl(reflect.TypeOf('⏰'))
-}
+// func main() {
+// 	pl(reflect.TypeOf(42))
+// 	pl(reflect.TypeOf(3.1415))
+// 	pl(reflect.TypeOf(3 + 4i))
+// 	pl(reflect.TypeOf("hello"))
+// 	pl(reflect.TypeOf('⏰'))
+// }
 	
+//Casting
+func main(){
+	cv1 := 1.5
+	cv2 := int(cv1)
+	pl(cv2)
+}
