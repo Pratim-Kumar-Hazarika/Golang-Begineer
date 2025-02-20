36089ac9c0fde3e65c7b10afc65fd23ebb14742d
Pratim <75177895+Pratim-Kumar-Hazarika@users.noreply.github.com>
2025-02-21 01:00:36 +0530

    add alias

--- hellogo.go
+++ hellogo.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"log"
-	"os"
-	"strings"
 )
 
+//alias
 var pl = fmt.Println
 
 func main() {
-	pl("What is your name??")
-	reader := bufio.NewReader(os.Stdin)
-	name, err := reader.ReadString('\n')
-	if err != nil {
-		log.Fatal(err)
-	}
-	name = strings.TrimSpace(name) // Removes the trailing newline
-	pl("Hello", name)
+	pl("Hello world")
 }
+	
