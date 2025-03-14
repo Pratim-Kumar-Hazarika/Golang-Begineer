b88f52fc9e08a907b6883b37a876916210b1e7d3
Pratim <hpro373@gmail.com>
2025-03-14 14:36:23 +0530

    Update main.go

--- 23jsondata/main.go
+++ 23jsondata/main.go
@@ -14,7 +14,8 @@ type course struct{
 }
 func main()  {
 	fmt.Println("Welcome to create a Json Data in golang")
-	EncodeJson()
+	// EncodeJson()
+	DecodeJson()
 }
 func EncodeJson(){
 	pratimCourses := []course{
@@ -29,4 +30,36 @@ func EncodeJson(){
 		panic(err)
 	}
 	fmt.Println(string(finalJson))
+}
+
+func DecodeJson(){
+	jsonDataFromWeb := []byte(`
+		{
+                "coursename": "Reactjs",
+                "price": 299,
+                "website": "prratimc.comn",
+                "tags": ["web-dev","js"]
+        }
+	`)
+	// var bestCourse course 
+
+	// checkValid := json.Valid(jsonDataFromWeb)
+	// if checkValid {
+	// 	fmt.Println("Json was valid")
+	// 	json.Unmarshal(jsonDataFromWeb ,&bestCourse)
+	// 	fmt.Printf("%#v\n", bestCourse)
+	// }else{
+	// 	fmt.Println("JSON WAS NOT VALID")
+	// }
+	/// some cases where you just want to add data to key value
+
+	
+	var myOnlineData map[string]interface{}
+	json.Unmarshal(jsonDataFromWeb, &myOnlineData)
+	fmt.Printf("%#v\n", myOnlineData)
+
+	for k,v := range myOnlineData {
+		fmt.Printf("Key is %v and value is %v and type is : %T\n", k,v,v)
+	}
+
 }
